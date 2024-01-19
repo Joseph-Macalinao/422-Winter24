aa794a27c648ffbfe89263d42d629c9d55fc9e48
Joseph Macalinao <jhmac121101@gmail.com>
2024-01-19 12:30:14 -0800

    changed some parts of inputs for queries

--- UI.py
+++ UI.py
@@ -5,7 +5,7 @@ root = tk.Tk()
 root.title("Grade Analysis")
 
 root.geometry("400x300")
-
+variable = tk.StringVar(root)
 def show_graph():
     dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 
@@ -18,30 +18,25 @@ def show_graph():
     plt.title('Median Salary (USD) by Age')
     plt.show()
 
-year = tk.Text(root, height=2, width=30)
-year.pack()
-year.insert(tk.END, "Please Enter Year")
-year_enter = tk.Entry(root)
-year_enter.pack()
-y = year_enter.get()
-
 depart = tk.Text(root, height=2, width=30)
 depart.pack()
 depart.insert(tk.END, "Please Enter Department")
-depart_enter = tk.Entry(root)
+depart_enter = tk.OptionMenu(root, variable, "Physics", "Biology", "Chemistry", "Earth Science", "Human Physiology")
 depart_enter.pack()
-d = depart_enter.get()
+#d = depart_enter.get()
 
 crn = tk.Text(root, height=2, width=30)
 crn.pack()
 crn.insert(tk.END, "Please Enter CRN")
 crn_enter = tk.Entry(root)
 crn_enter.pack()
-crn_enter.get()
+#crn_enter.get()
 
-variable = tk.StringVar(root)
+
 variable.set("None")
-
+choice = tk.Text(root, height=2, width=30)
+choice.pack()
+choice.insert(tk.END, "A or Passing Distribution")
 w = tk.OptionMenu(root, variable, "A distribution", "Pass distribution")
 w.pack()
 
