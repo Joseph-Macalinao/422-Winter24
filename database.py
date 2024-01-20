c1b546f9142dd75d60eec45b28847b5c5f0524f8
Nathan Koga <103086476+nathankoga@users.noreply.github.com>
2024-01-20 12:53:50 -0800

    Added Specific functions to db and query modules

--- database.py
+++ database.py
@@ -5,14 +5,28 @@ Primarily used by the Query Handler.
 
 # define functions required to search the database, including the following:
 
-def class_search():
+def class_search(crn, all_instructors, easy_a):
+    """
+    Given a crn, search for all professors who have taught that class.
+    """
     pass
 
-def department_search():
+def department_search(dept, all_instructors, easy_a):
+    """
+    Given a department, search for information regarding all professors
+    of that specific department.
+    """
     pass
 
-def level_department_search():
+def level_department_search(level, dept, all_instructors, easy_a):
+    """
+    Given a level and department, search for all professors teaching at that class level.
+    """
     pass
 
-def class_level_search():
+def class_level_search(level, dept, all_instructors, easy_a):
+    """
+    Given a level and department, search for all classes of that 
+    particular level in that department.
+    """
     pass
--- query_handler.py
+++ query_handler.py
@@ -1,27 +1,65 @@
 """
 Module containing functions for handling and resolving queries.
-Primarily used by the standard UI.
+Primarily used by the standard UI:
+    UI gathers information necessary and creates a Query object
 """
+
+from database import *
 
 class Query:
     """
     Query objects contain all specifications required to fully identify a query.
 
     Parameters:
-        main_request: Enumerated identifier for (0) specific class, (1) specific department, 
-                                            (2) level in department, and (3) all classes of given level                                  
+        main_request (int): Enumerated identifier for (0) specific class, (1) specific department, 
+                                            (2) professors teaching at given level in department, 
+                                            and (3) all classes of a given level                                  
         
-        meta_data: Data to satisfy actual query, such as actual CRN or class level
-                    (may need to extend datatype)
+        meta_data (list[mixed]): Data to satisfy actual query, such as CRN or class level
         
-        all_instructors: Boolean identifying "all instructors" vs "regular faculty"
-        easy_a: Boolean identifying % of A's vs % of D/F's
+        all_instructors (bool): Boolean identifying "all instructors" vs "regular faculty"
+        easy_a (bool): Boolean identifying % of A's vs % of D/F's
 
     Returns:
         something 
     """
     def __init__(self, req, md, instr, easyA):
         self.main_request = req
-        self.meta_data = md
+        self.meta_data = md   # might need to be a list of 1+ elements
         self.all_instructors = instr
         self.easy_a = easyA
+
+
+    def database_search(self):
+        """
+        High level function: 
+        Calls specific database searches (as defined in database.py)
+        
+        --> is this bad coding??? feels kinda weird idk
+        """
+        
+        if self.main_request == 0:
+            class_search(self.meta_data, self.all_instructors, self.easy_a) 
+
+        elif self.main_request == 1:
+            department_search(self.meta_data, self.all_instructors, self.easy_a) 
+
+        elif self.main_request == 2:
+            level_department_search(self.meta_data, self.all_instructors, self.easy_a) 
+
+        elif self.main_request == 3:
+            class_level_search(self.meta_data, self.all_instructors, self.easy_a) 
+
+        else:
+            # error handling lol
+            pass
+
+
+    """
+    TODO: need to figure out what the UI needs returned in order to make a graph
+    idea:
+        class GraphInformation:
+        --> class object to be made in the UI.py file (and imported into this file)
+        --> GraphInformation object returned to UI when a query is satified by handler
+        --> then UI.py can take graph information and display 
+    """
