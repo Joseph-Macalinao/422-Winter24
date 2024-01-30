44961fd0acf93788212de13d6333a85ea312caba
Nathan Koga <103086476+nathankoga@users.noreply.github.com>
2024-01-29 22:31:03 -0800

    Database: All search prototypes completed, along with examples of query_handler making query requests to the database.

--- database.py
+++ database.py
@@ -15,7 +15,6 @@ def class_search(class_num, dept, alL_instructors, easy_a):
     Given a class number and a department, search for (a % or d/f %) of all professors who have taught that class.
     """
     # classes (a list w/ dicts for each class), Regular_Faculty (a list of faculty members)
-    
     returning_data = {}  # key = professor, val = [percentage, num_entries]  (since tuples are immutable)
     class_to_find = dept + str(class_num)  # e.g., "CIS110"
     department_info_dict = data_bank[dept]  # attempt to load database entry for given dept
@@ -107,22 +106,52 @@ def level_department_search(class_level, dept, alL_instructors, easy_a):
     """
     Given a class level and department, search for all professors teaching at that class level.
     """
-    matching_professors = []
-    classlevel = dept + str(class_level)
-    classmax = dept + str(class_level + 100)
-    for department_entry in easy_a["departments"]:
-        for department_code, department_data in department_entry.items():
-            if department_code == dept:
-                for class_entry in department_data["classes"]:
-                    for class_code, class_data in class_entry.items():
-                        if class_code > classlevel and class_code < classmax:
-                            for entry in class_data:
-                                if entry["instructor"] in matching_professors:
-                                        pass
-                                else: 
-                                    matching_professors.append(entry["instructor"])
-                                    
-    return matching_professors
+    returning_data = {}  # key = professor, val = [percentage, num_entries]  (since tuples are immutable)
+    department_info_dict = data_bank[dept]  # attempt to load database entry for given dept
+
+    class_max = dept + str(class_level + 100)
+    class_level = dept + str(class_level)
+
+    if department_info_dict:  # department exists
+        class_list = department_info_dict["classes"]  # list of classes (ofc)
+        for class_obj in class_list: 
+
+            for class_code, class_data in class_obj.items():
+                # for each class_code, determine if it's in correct range (i.e. 100~199)
+                if class_code >= class_level and class_code < class_max:
+                    for class_instance in class_data:
+                        instructor_name = class_instance["instructor"]
+                        # TODO: figure out toggle for regular faculty 
+                        
+                        if instructor_name not in returning_data:  # first_time entry --> make dict entry
+                            returning_data[instructor_name] = [0.0,0] 
+
+                        # gather running sum of desired percentage and frequency (for final avg)
+                        if easy_a:  
+                            returning_data[instructor_name][1] += 1
+                            returning_data[instructor_name][0] += float(class_instance["aprec"])
+                        else:
+                            returning_data[instructor_name][1] += 1
+                            returning_data[instructor_name][0] += float(class_instance["dprec"])
+                            returning_data[instructor_name][0] += float(class_instance["fprec"])
+                    else:
+                        # print(f"Invalid class search: {class_to_find} not found")
+                        pass
+                else:
+                    # class is not in the correct level (i.e., not between 100~199)
+                    pass
+    else:
+        # print(f"Invalid department search: {dept} not found")
+        pass
+
+    if returning_data:  # if returning data has entries, we need to find avg
+        for professor in returning_data:
+            curr_dict = returning_data[professor]
+            if easy_a:
+                curr_dict[0] = round(curr_dict[0] / curr_dict[1], 2)
+            else:  # twice as many data points as classes --> divide by 2* class_num
+                curr_dict[0] = round(curr_dict[0] / (curr_dict[1]) * 2, 2)
+    return returning_data 
 
 
 def class_level_search(class_level, dept, alL_instructors, easy_a):
@@ -130,18 +159,52 @@ def class_level_search(class_level, dept, alL_instructors, easy_a):
     Given a class level and department, search for all classes of that 
     particular level in that department.
     """
-    matching_classes = []
-    classlevel = dept + str(class_level)
-    classmax = dept + str(class_level + 100)
-    for department_entry in easy_a["departments"]:
-        for department_code, department_data in department_entry.items():
-            if department_code == dept: 
-                for class_entry in department_data["classes"]:
-                    for class_code, class_data in class_entry.items():
-                        if class_code > classlevel and class_code < classmax:
-                            matching_classes.append(class_data)
 
-    return matching_classes
+    returning_data = {}  # key = class name, val = [percentage, num_entries]  (since tuples are immutable)
+    department_info_dict = data_bank[dept]  # attempt to load database entry for given dept
+
+    class_max = dept + str(class_level + 100)
+    class_level = dept + str(class_level)
+
+    if department_info_dict:  # department exists
+        class_list = department_info_dict["classes"]  # list of classes (ofc)
+        for class_obj in class_list: 
+
+            for class_code, class_data in class_obj.items():
+                # for each class_code, determine if it's in correct range (i.e. 100~199)
+                if class_code >= class_level and class_code < class_max:
+                    for class_instance in class_data:
+                        # TODO: figure out toggle for regular faculty 
+                        
+                        if class_code not in returning_data:  # first_time entry --> make dict entry
+                            returning_data[class_code] = [0.0,0] 
+
+                        # gather running sum of desired percentage and frequency (for final avg)
+                        if easy_a:  
+                            returning_data[class_code][1] += 1
+                            returning_data[class_code][0] += float(class_instance["aprec"])
+                        else:
+                            returning_data[class_code][1] += 1
+                            returning_data[class_code][0] += float(class_instance["dprec"])
+                            returning_data[class_code][0] += float(class_instance["fprec"])
+                    else:
+                        # print(f"Invalid class search: {class_to_find} not found")
+                        pass
+                else:
+                    # class is not in the correct level (i.e., not between 100~199)
+                    pass
+    else:
+        # print(f"Invalid department search: {dept} not found")
+        pass
+
+    if returning_data:  # if returning data has entries, we need to find avg
+        for professor in returning_data:
+            curr_dict = returning_data[professor]
+            if easy_a:
+                curr_dict[0] = round(curr_dict[0] / curr_dict[1], 2)
+            else:  # twice as many data points as classes --> divide by 2* class_num
+                curr_dict[0] = round(curr_dict[0] / (curr_dict[1]) * 2, 2)
+    return returning_data 
 
 
 def print_class_info():
@@ -151,12 +214,19 @@ def print_class_info():
 
 
 if __name__ == "__main__":
+
     # query testing (probably remove for final product)
     # easy_a_res = class_search(122, "CIS", True, True)
     # pass_res = class_search(122, "CIS", True, False)
     
-    easy_a_res = department_search("CIS", True, True)
-    pass_res = department_search("CIS", True, False)
+    # easy_a_res = department_search("CIS", True, True)
+    # pass_res = department_search("CIS", True, False)
+    
+    # easy_a_res = level_department_search(600, "CIS", True, True)
+    # pass_res = level_department_search(600, "CIS", True, False)
+    
+    easy_a_res = class_level_search(600, "CIS", True, True)
+    pass_res = class_level_search(600, "CIS", True, False)
     # goal --> get res to print relevant data to send back to the query handler
     print(easy_a_res)
     print(pass_res)
