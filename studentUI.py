4e5fe281df7d77a6c60965de54707a1e5e864510
Anna Finlay <93169390+annafinlay@users.noreply.github.com>
2024-02-04 15:21:03 -0800

    Update studentUI.py

--- studentUI.py
+++ studentUI.py
@@ -27,6 +27,12 @@ variable4.set("None")
 global crnEnter
 crnEnter = tk.StringVar(root)
 crnEnter.set("None")
+global selected_query
+
+
+
+
+
 def admin():
     ''' show admin pop out page '''
     adminView()
@@ -54,6 +60,34 @@ def name_dropdown_input(new_item, curr_root, frame_name, var_name, input_text, o
     var_output = var_name
 
     return var_output
+
+
+def graph_my_output_list(output_list: list)-> None: #graphs the output list
+    
+    print(output_list) # has 5 variables
+    a_vs_justpass = (output_list[2] == 'A distribution')
+    if (output_list[2] == ""):
+        a_vs_justpass = True
+    if (output_list[1] == ""):
+        output_list[1] = 0
+    else:
+        while(True):
+            try:
+                output_list[1] = int(output_list[1])
+                output_list[3] = int(output_list[3])
+                break
+            except:
+                pass
+        
+    my_query = Query(2, a_vs_justpass, True, class_level = output_list[1], dept=output_list[0])
+    Dict1 = my_query.database_search()
+    print(Dict1)
+    plotter(2, a_vs_justpass, True, Dict1, output_list[1], output_list[0])
+    print(output_list)
+
+
+
+
 
 def query_selected_option(query, v1, v2, v3, v4):
 
@@ -152,26 +186,9 @@ def query_selected_option(query, v1, v2, v3, v4):
         output_list.append(variable2.get())
         output_list.append(variable3.get())
         output_list.append(variable4.get())
-        print(output_list) # has four variables
-        a_vs_justpass = (output_list[2] == 'A distribution')
-        if (output_list[2] == ""):
-            a_vs_justpass = True
-        if (output_list[1] == ""):
-            output_list[1] = 0
-        else:
-            while(True):
-                try:
-                    output_list[1] = int(output_list[1])
-                    output_list[3] = int(output_list[3])
-                    break
-                except:
-                    pass
-        
-        my_query = Query(2, a_vs_justpass, True, class_level = output_list[1], dept=output_list[0])
-        Dict1 = my_query.database_search()
-        print(Dict1)
-        plotter(2, a_vs_justpass, True, Dict1, output_list[1], output_list[0])
-        print(output_list)
+        output_list.append(selected_query )
+    
+        graph_output_list(output_list) #********* here we are graphing what we have *************#
 
     enterButton = tk.Button(root, bg='light blue', fg='black', text="Enter", font=('Bold 24'), command=output)
     #enterButton.configure(bg="blue")
@@ -228,6 +245,7 @@ def main():
     now_item.configure(bg="gray92", fg="black", highlightthickness=0, borderwidth=0)
     selected = name_dropdown_input(now_item, tk, query_selection, variable5, "Enter Query Type", ["Specific Class", "Department", "Department Level by Teacher", "Department Level by Class"])
     def selection():
+        selected_query = selected.get()
         x = variable5.get()
         query_selected_option(x, variable1, variable2, variable3, variable4)
         
@@ -286,55 +304,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-'''
-# dept type input
-    deptframe = Frame(root) 
-    deptframe.place(x=360, y=250)
-    deptframe.configure(bg="gray92")
-    tmp_item = tk.Text(deptframe, height=0, width=21, font=('Bold', 17))
-    tmp_item.configure(bg="gray92", fg='black',highlightthickness = 0, borderwidth=0)
-    tmp = name_dropdown_input(tmp_item, tk, deptframe, variable1, "Please Enter Department", ["Biochemistry", "Bioengineering", "Biology", "Chemistry", "CIT", "CIS", "Data Science", "Environmental Studies", "Human Physiology", "Mathematics", "MACS", "Multidiscinary Science", "Neuroscience", "Physics", "Psychology"])
-    variable1 = tmp
-
-    # crn type input
-    crnframe = Frame(root)
-    crnframe.place(x=360, y=288)
-    crnframe.configure(bg="gray92")
-    new_item = tk.Text(crnframe, height=0, width=34, font=('Bold', 17)) 
-    new_item.pack(side=LEFT)
-    new_item.config(state="normal")
-    new_item.insert(tk.END, "Class Number")
-    new_item.config(state="disabled", bg="gray92", fg='black', highlightthickness = 0, borderwidth=0)
-    crn_enter = tk.Entry(crnframe,width=12,font=("Helvetica 17 italic"), fg='grey60')
-    #lambda func to just get rid of text in class number when entering
-    crn_enter.bind("<Button-1>",lambda e: crn_enter.delete(0,tk.END))
-    crn_enter.insert(0, "Class Number")
-    #crn_enter.configure(bg="gray92", highlightthickness = 1, borderwidth=0)
-    crn_enter.pack(side=RIGHT)
-
-    # type of grading input
-    distframe = Frame(root) 
-    distframe.place(x=360, y=328)
-    distframe.config(bg="gray92")
-    tmp_item = tk.Text(distframe, height=0, width=26, font=('Bold', 17), bg="gray92", fg='black',highlightthickness = 0, borderwidth=0)
-    tmp = name_dropdown_input(tmp_item, tk, distframe, variable2, "A or Passing Distribution", ["A distribution", "Pass distribution"])
-    variable2 = tmp
-
-    # level input
-    levelframe = Frame(root)
-    levelframe.place(x=360, y=366)
-    levelframe.configure(bg="gray92")
-    tmp_item = tk.Text(levelframe, height=0, width=39, font=('Bold', 17), bg="gray92", fg='black', highlightthickness = 0, borderwidth=0)
-    tmp = name_dropdown_input(tmp_item, tk, levelframe, variable3, "Level", ["None", "100", "200", "300", "400"])
-    #if variable5 == "Specific Class":
-    #    tmp_item.configure(state="disabled")
-    variable3 = tmp
-
-    # level input
-    allframe = Frame(root)
-    allframe.place(x=360, y=410)
-    allframe.configure(bg="gray92")
-    tmp_item = tk.Text(allframe, height=0, width=28, font=('Bold', 17), bg="gray92", fg='black', highlightthickness = 0, borderwidth=0)
-    tmp = name_dropdown_input(tmp_item, tk, allframe, variable4, "All Instructors/Regular Faculty", ["All Instructors", "Regular Faculty"])
-    variable4 = tmp'''
